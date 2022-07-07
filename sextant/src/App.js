75753e6df12ca0658ac824d7f538f20b8fe02caf
Aman Singh Sikarwar <87236656+AMAN02SS@users.noreply.github.com>
2022-07-07 23:27:39 +0530

    2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import AddressFinder from './AddressFinder';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exihbit';
 
+
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Banner bannerText="Sextant" />
-                <Exhibit name="exhibit! 1"></Exhibit>
-                <Exhibit name="exhibit! 2"></Exhibit>
-                <Exhibit name="exhibit! 3"></Exhibit>
-            </div>
+          <div className="App">
+          <Banner bannerText="Sextant" />
+          <Exhibit name="Public IPv4 Address">
+              <AddressFinder url='https://api.ipify.org?format=json' />
+          </Exhibit>
+          <Exhibit name="Public IPv6 Address">
+              <AddressFinder url='https://api64.ipify.org?format=json' />
+          </Exhibit>
+      </div>
         );
     }
 }
