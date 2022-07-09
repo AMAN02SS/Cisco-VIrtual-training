8cfd9069d0a83b5a2d9622e1013f16245ce5f3df
Aman Singh Sikarwar <87236656+AMAN02SS@users.noreply.github.com>
2022-07-09 21:25:43 +0530

    4th submit

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,7 @@ import AddressFinder from './AddressFinder';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exihbit';
+import PylonConnector from './PylonConnector';
 
 
 class App extends Component {
@@ -15,6 +16,9 @@ class App extends Component {
           </Exhibit>
           <Exhibit name="Public IPv6 Address">
               <AddressFinder url='https://api64.ipify.org?format=json' />
+          </Exhibit>
+          <Exhibit name="Pylon Packet Latency">
+                    <PylonConnector />
           </Exhibit>
       </div>
         );
--- sextant/src/Banner.js
+++ sextant/src/Banner.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Banner.css';
+import logo from './sextant.png';
 
 class Banner extends Component {
     render() {
         return (
             <div className="Banner">
-                <h1>{this.props.bannerText}</h1>
+                <img src={logo} alt="" height="50px" width="50px" margin=""/>
+                <h1>{this.props.bannerText} </h1>
             </div>
         );
     }
